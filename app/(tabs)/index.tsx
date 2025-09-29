ae28fbd09bfbd093e46911357ce774955262f105
Nasim <mdnasimhosen333@gmail.com>
2025-09-29 23:37:33 +0600

    init

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-bold text-dark-200">
+        Welcome to Movie App!
+      </Text>
+    </View>
+  );
+}
