8017259afecf20301afbd952ed13b370d612f70f
Nasim <mdnasimhosen333@gmail.com>
2025-09-30 00:33:45 +0600

    refactor: remove MovieDetails screen and update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
     </Stack>
   );
 }
